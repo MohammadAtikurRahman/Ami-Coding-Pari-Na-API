24ddb1ad5c49fc2c0cdae177cf72a4f9f03b5c40
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-21 09:55:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+
+//import routes
+
+const authRoute  = require('./routes/auth')
+
+app.listen(3000, () => console.log('Server up and running'));
---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.postt('/register', (req,res) => {
+
+
+  res.send('Register page');
+
+})
+
+
+module.exports = router;
